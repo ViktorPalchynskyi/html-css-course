44e7d013ec01b48d91167e26de7f9567c13553cc
Arlecuno <palchinskij19@gmail.com>
2020-07-05 00:20:48 +0300

    study js string type

---
+++ js/youtube_course/basic/03_string.js
@@ -0,0 +1,63 @@
+// const name = 'Viktor'
+// const age = 22
+
+
+
+// const output = 'Hello, my name is ' + name + '. I\'m ' + age + ' years old.'
+
+// function getAge() {
+//    return age
+// }
+
+// const output = `Hello, my name is ${name} I\'m ${getAge() > 20 ? getAge() : 'Error'} y.o.`
+// const output = `
+//    <div>This is dic</div>
+//    <p>This is p</p>
+// `
+
+
+
+
+
+// console.log(output)
+
+
+// const name = 'Viktor'
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('tor'))
+// console.log(name.startsWith('tor'))
+// console.log(name.toLowerCase().startsWith('vik'))
+// console.log(name.startsWith('Vik'))
+// console.log(name.endsWith('tor'))
+// console.log(name.repeat(3))
+
+// const string = '       password       '
+
+// console.log(string.trim())
+// console.log(string.trimLeft())
+// console.log(string.trimRight())
+
+
+// function logPerson(s, name, age) {
+//    if (age <= 0) {
+//       age = 'It\'s not born yet!'
+//    }
+//    return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+
+
+// const personName = 'Viktor'
+// const personName2 = 'Misha'
+// const personAge = 22
+// const personAge2 = 0
+
+
+// const output = logPerson`Name: ${personName}, Age: ${personAge}!`
+// const output2 = logPerson`Name: ${personName2}, Age: ${personAge2}!`
+
+
+// console.log(output)
+// console.log(output2)
