2e0e9945ffba99abd4a1cc192b1aa39129a8f42a
Arlecuno <palchinskij19@gmail.com>
2020-07-04 22:56:27 +0300

    study js number type

---
+++ js/youtube_course/basic/02_numbers.js
@@ -0,0 +1,74 @@
+// 1 Number
+
+// const num = 42
+// const float = 42.2
+// const pow = 10e3
+
+
+
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
+// console.log('MAX_VALUE', Number.MAX_VALUE)
+// console.log('MIN_VALUE', Number.MIN_VALUE)
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
+// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
+// console.log(Math.pow(2, 53) - 1)
+// console.log('2/ 0', 2 / 0)
+// const weird = 2 / undefined
+// console.log(Number.NaN)
+// console.log(isNaN(weird))
+
+// const stringInt = '42'
+// const stringFloat = '42.2'
+
+// console.log(parseInt(stringInt) + 2)
+// console.log(Number(stringInt) + 2)
+// console.log(+stringInt + 2)
+
+// console.log(parseFloat(stringFloat) + 10)
+// console.log(+stringFloat + 10)
+
+
+
+// console.log(num)
+// console.log(pow)
+
+// console.log(0.4 + 0.2)
+// console.log(+(0.4 + 0.2).toFixed(1))
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
+
+
+// 2 BigInt
+
+// console.log(9007199254740991000000000n - 90071992547409910000000n)
+
+
+// 3 Math
+
+// console.log(Math.E)
+// console.log(Math.PI)
+
+
+
+
+
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(5, 2))
+// console.log(Math.abs(-21))
+// console.log(Math.max(29, 243, 124, 4441))
+// console.log(Math.min(29, 243, 124, 4441))
+// console.log(Math.floor(4.8))
+// console.log(Math.ceil(4.1))
+// console.log(Math.round(4.8))
+// console.log(Math.trunc(4.8))
+// console.log(Math.random(4.8))
+
+
+// 4 Example
+
+// function getRandomBetween(min, max) {
+//    return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+
+// console.log(getRandomBetween(10, 42))
