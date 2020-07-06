e31fed9da2e277e97d39309d1981500ac1699cb5
Arlecuno <palchinskij19@gmail.com>
2020-07-06 14:33:09 +0300

    study js prototype

---
+++ js/youtube_course/advance_topics/01_prototype.js
@@ -0,0 +1,30 @@
+// const person = {
+//    name: 'Maxim',
+//    age: 25,
+//    greet: () => {
+//       console.log('Greet!')
+//    }
+// }
+
+
+
+
+const person = new Object({
+   name: 'Maxim',
+   age: 25,
+   greet: () => {
+      console.log('Greet!')
+   }
+})
+
+
+Object.prototype.sayHello = () => {
+   console.log('Hello!')
+}
+
+
+const lena = Object.create(person)
+
+lena.name = 'Elena'
+
+const str = new String('I am string')
