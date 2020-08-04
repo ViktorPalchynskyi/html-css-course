8cbf3b649514a2e99eec609b147358bba0e080a4
Arlecuno <palchinskij19@gmail.com>
2020-08-04 14:22:30 +0300

    finish lesson 3, study data type

---
+++ js/udemy_course/Module_1/lesson_3/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4.5;
+
+console.log(-4 / 0);
+console.log('string' * 9);
+
+const person = "Viktor";
+
+
+const bool = true;
+
+let und;
+
+console.log(und);
+
+
+const obj = {
+   name: "Josh",
+   age: 25,
+   isMarried: false
+};
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
