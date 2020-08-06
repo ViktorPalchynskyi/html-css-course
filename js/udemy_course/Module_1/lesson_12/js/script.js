6940a4a1590464e8bd508a1bdc10aa3270783141
Arlecuno <palchinskij19@gmail.com>
2020-08-06 14:30:46 +0300

    finish lesson 12, study string and number methods

---
+++ js/udemy_course/Module_1/lesson_12/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const str = 'TeSt';
+
+const arr = [1, 2, 4];
+
+console.log([2]);
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello World';
+
+console.log(logg.slice(6));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
