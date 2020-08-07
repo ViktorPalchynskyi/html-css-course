a3cc9848cb01c880e28a5304472295defefe2033
Arlecuno <palchinskij19@gmail.com>
2020-08-07 23:03:54 +0300

    finish lesson 21

---
+++ js/udemy_course/Module_1/lesson_21/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// let x = 5;
+// alert(x++);
+
+// console.log(typeof ([] + false - null));
+// console.log(([] + false - null + true));
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// console.log([] + 1 + 2);
+
+// alert("1" [0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+console.log(!!(1 && 2) === (1 && 2));
+
+// alert(null || 2 && 3 || 4);
+
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+
+console.log(a === b);
+
+// alert(+"Infinity");
+
+console.log(typeof (+"Infinity"));
+
+console.log("Ёжик" > "яблоко");
+
+console.log(0 || "" || 2 || undefined || true || false);
