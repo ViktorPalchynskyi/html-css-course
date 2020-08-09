fa7f3cb55b210ee1169c99d8f5e07a731e686e75
Arlecuno <palchinskij19@gmail.com>
2020-08-09 23:11:02 +0300

    finish lesson 28, study mobile web methods

---
+++ js/udemy_course/Module_1/lesson_28/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//toucheleave
+//touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+   const box = document.querySelector('.box');
+   box.addEventListener('touchstart', (e) => {
+      e.preventDefault();
+
+      console.log('start');
+      console.log(e.targetTouches);
+   });
+
+   box.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+
+      console.log(e.targetTouches[0].pageX);
+   });
+
+   // box.addEventListener('touchend', (e) => {
+   //    e.preventDefault();
+
+   //    console.log('end');
+   // });
+});
+
+// touches
+// targetTouches
+// changedTouches
