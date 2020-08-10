bd54adbc4959d27689af68dfc02656ab7c52fdc1
Arlecuno <palchinskij19@gmail.com>
2020-08-10 17:42:53 +0300

    finish lesson 33, study Data type and methods

---
+++ js/udemy_course/Module_2/lesson_33/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const now = new Date('2020-08-10');
+// new Date.parse('2020-08-10');
+
+
+
+console.log(now.setHours(18, 42));
+console.log(now);
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+   let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end- start} миллисекунд`);
