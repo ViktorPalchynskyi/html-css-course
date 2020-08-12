ea579bc95396eecf356eb63df0cceebc9c844c2d
Arlecuno <palchinskij19@gmail.com>
2020-08-12 16:30:17 +0300

    finish lesson 35, study document and window methods

---
+++ js/udemy_course/Module_2/lesson_35/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const box = document.querySelector('.box'),
+   btn = document.querySelector('button');
+
+// const width = box.clientWidth;
+// const heigth = box.clientHeight;
+
+// const width = box.offsetWidth;
+// const heigth = box.offsetHeight;
+
+const width = box.scrollWidth;
+const heigth = box.scrollHeight;
+
+console.log(width, heigth);
+
+btn.addEventListener('click', () => {
+   // box.style.height = box.scrollHeight + 'px';
+   console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+
+console.log(style.display);
