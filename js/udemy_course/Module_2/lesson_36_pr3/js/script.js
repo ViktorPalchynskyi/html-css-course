8737d0720d4cbb5dbd27cecd7b0a8b7cbc111ade
Arlecuno <palchinskij19@gmail.com>
2020-08-14 19:41:42 +0300

    finish lesson 37, add modification to modal window

--- js/udemy_course/Module_2/lesson_36_pr3/js/script.js
+++ js/udemy_course/Module_2/lesson_36_pr3/js/script.js
@@ -94,17 +94,22 @@ window.addEventListener('DOMContentLoaded', () => {
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
 
-   modalTrigger.forEach(item => {
-      item.addEventListener('click', () => {
-         modal.classList.toggle('show');
-         document.body.style.overflow = 'hidden';
-      });
-   });
+
+   function openModal() {
+      modal.classList.toggle('show');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
+   }
 
    function closeModal() {
       modal.classList.toggle('show');
       document.body.style.overflow = '';
    }
+
+
+   modalTrigger.forEach(item => {
+      item.addEventListener('click', openModal);
+   });
 
    modalCloseBtn.addEventListener('click', closeModal);
 
@@ -119,4 +124,15 @@ window.addEventListener('DOMContentLoaded', () => {
          closeModal();
       }
    });
+
+   const modalTimerId = setTimeout(openModal, 5000);
+
+   function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+         openModal();
+         window.removeEventListener('scroll', showModalByScroll);
+      }
+   }
+
+   window.addEventListener('scroll', showModalByScroll);
 });
