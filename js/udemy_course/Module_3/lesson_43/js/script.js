5e188042d4fbbb2e228be266e978fae3b8a56ce3
Arlecuno <palchinskij19@gmail.com>
2020-08-23 21:20:42 +0300

    finished lesson 43, study AJAX

---
+++ js/udemy_course/Module_3/lesson_43/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+   inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+   const request = new XMLHttpRequest();
+
+   request.open('GET', 'js/current.json');
+   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+   request.send();
+
+
+   request.addEventListener('load', () => {
+      if (request.status === 200) {
+         console.log(request.response);
+         const data = JSON.parse(request.response);
+         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(3);
+      } else {
+         inputUsd.value = 'Something goes wrong!';
+      }
+   });
+   // status
+   // statusText
+   // response
+   // readyState
+
+
+});
