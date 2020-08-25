42652e51875d3e6de11107fa6145283fd5b46c76
Arlecuno <palchinskij19@gmail.com>
2020-08-25 21:49:26 +0300

    finished lesson 51, study axion library

--- js/udemy_course/Module_3/lesson_49_50_51_pr3/js/script.js
+++ js/udemy_course/Module_3/lesson_49_50_51_pr3/js/script.js
@@ -192,54 +192,40 @@ window.addEventListener('DOMContentLoaded', function () {
       return await res.json();
    };
 
-   // getResource('http://localhost:3000/menu')
-   //    .then(data => {
-   //       data.forEach(({
-   //          img,
-   //          altimg,
-   //          title,
-   //          descr,
-   //          price
-   //       }) => {
-   //          new MenuCard(img,
-   //             altimg,
-   //             title,
-   //             descr,
-   //             price, '.menu .container').render();
-   //       });
-   //    });
-
    getResource('http://localhost:3000/menu')
-      .then(data => createCard(data));
-
-   function createCard(data) {
-      data.forEach(
-         ({
+      .then(data => {
+         data.forEach(({
             img,
             altimg,
             title,
             descr,
             price
          }) => {
-            const element = document.createElement('div');
-            const priceUan = price * 27;
+            new MenuCard(img,
+               altimg,
+               title,
+               descr,
+               price, '.menu .container').render();
+         });
+      });
 
-            element.classList.add('menu__item');
-            element.innerHTML = `
-            <img src=${img} alt=${altimg}>
-            <h3 class="menu__item-subtitle">${title}</h3>
-            <div class="menu__item-descr">${descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-               <div class="menu__item-cost">Цена:</div>
-               <div class="menu__item-total"><span>${priceUan}</span> грн/день</div>
-            </div>
-            `;
-            document.querySelector('.menu .container').append(element);
-         }
-      );
-   }
 
+   axios.get('http://localhost:3000/menu')
+      .then(data => {
+         data.forEach(({
+            img,
+            altimg,
+            title,
+            descr,
+            price
+         }) => {
+            new MenuCard(img,
+               altimg,
+               title,
+               descr,
+               price, '.menu .container').render();
+         });
+      });
    // Forms
 
    const forms = document.querySelectorAll('form');
