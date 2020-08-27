a60f84024b93a0bada721d94f7bce0feb065b8e7
Arlecuno <palchinskij19@gmail.com>
2020-08-27 11:58:32 +0300

    finished lesson 57, study regular expration and add regExp to the project

--- js/udemy_course/Module_3/lesson_52_53_54_pr3/js/script.js
+++ js/udemy_course/Module_3/lesson_52_53_54_pr3/js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
    // Timer
 
-   const deadline = '2020-09-11';
+   const deadline = '2020-05-11';
 
    function getTimeRemaining(endtime) {
       const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -282,7 +282,7 @@ window.addEventListener('DOMContentLoaded', function () {
       }, 4000);
    }
 
-   //Slider
+   // Slider
 
    let offset = 0;
    let slideIndex = 1;
@@ -321,37 +321,37 @@ window.addEventListener('DOMContentLoaded', function () {
       dots = [];
    indicators.classList.add('carousel-indicators');
    indicators.style.cssText = `
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        z-index: 15;
-        display: flex;
-        justify-content: center;
-        margin-right: 15%;
-        margin-left: 15%;
-        list-style: none;
-    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+       position: absolute;
+       right: 0;
+       bottom: 0;
+       left: 0;
+       z-index: 15;
+       display: flex;
+       justify-content: center;
+       margin-right: 15%;
+       margin-left: 15%;
+       list-style: none;
+   `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
    slider.append(indicators);
 
    for (let i = 0; i < slides.length; i++) {
       const dot = document.createElement('li');
       dot.setAttribute('data-slide-to', i + 1);
       dot.style.cssText = `
-            box-sizing: content-box;
-            flex: 0 1 auto;
-            width: 30px;
-            height: 6px;
-            margin-right: 3px;
-            margin-left: 3px;
-            cursor: pointer;
-            background-color: #fff;
-            background-clip: padding-box;
-            border-top: 10px solid transparent;
-            border-bottom: 10px solid transparent;
-            opacity: .5;
-            transition: opacity .6s ease;
-        `;
+           box-sizing: content-box;
+           flex: 0 1 auto;
+           width: 30px;
+           height: 6px;
+           margin-right: 3px;
+           margin-left: 3px;
+           cursor: pointer;
+           background-color: #fff;
+           background-clip: padding-box;
+           border-top: 10px solid transparent;
+           border-bottom: 10px solid transparent;
+           opacity: .5;
+           transition: opacity .6s ease;
+       `;
       if (i == 0) {
          dot.style.opacity = 1;
       }
@@ -360,10 +360,10 @@ window.addEventListener('DOMContentLoaded', function () {
    }
 
    next.addEventListener('click', () => {
-      if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+      if (offset == (deleteNotDigits(width) * (slides.length - 1))) {
          offset = 0;
       } else {
-         offset += +width.slice(0, width.length - 2);
+         offset += deleteNotDigits(width);
       }
 
       slidesField.style.transform = `translateX(-${offset}px)`;
@@ -386,9 +386,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
    prev.addEventListener('click', () => {
       if (offset == 0) {
-         offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+         offset = deleteNotDigits(width) * (slides.length - 1);
       } else {
-         offset -= +width.slice(0, width.length - 2);
+         offset -= deleteNotDigits(width);
       }
 
       slidesField.style.transform = `translateX(-${offset}px)`;
@@ -414,7 +414,7 @@ window.addEventListener('DOMContentLoaded', function () {
          const slideTo = e.target.getAttribute('data-slide-to');
 
          slideIndex = slideTo;
-         offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+         offset = deleteNotDigits(width) * (slideTo - 1);
 
          slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -429,4 +429,7 @@ window.addEventListener('DOMContentLoaded', function () {
       });
    });
 
+   function deleteNotDigits(str) {
+      return +str.replace(/\D/g, '');
+   }
 });
---
+++ js/udemy_course/Module_3/lesson_56/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// const ans = prompt('Введите ваше число');
+
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+
+console.log(str.match(/\D/ig));
+
+
+// \D
+// \D
+// \d - digits
+// \w - words 
+// \s - spaces
+
+// i
+// g
+// m
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/\./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
