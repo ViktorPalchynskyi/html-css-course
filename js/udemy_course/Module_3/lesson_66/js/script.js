e82234a77920ab4af0c0adfe8994742eb25a392a
Arlecuno <palchinskij19@gmail.com>
2020-09-03 17:39:59 +0300

    finished lesson 66, study babel

---
+++ js/udemy_course/Module_3/lesson_66/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);\
+
+// const answer = +prompt("Are you 18 y.o?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('That is your name?', '');
+// answers[1] = prompt('That is your surname?', '');
+// answers[2] = prompt('How old are your?', '');
+
+// document.write(answers);
+
+console.log(typeof (answers));
+console.log(typeof (null));
