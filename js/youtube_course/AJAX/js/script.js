205ca9998172f2dce0c6ea3dad693a51c974ee5e
Arlecuno <palchinskij19@gmail.com>
2020-09-23 12:47:45 +0300

    work with fetch API and axios

--- js/youtube_course/AJAX/js/script.js
+++ js/youtube_course/AJAX/js/script.js
@@ -1,44 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
 
    function req() {
-      const request = new XMLHttpRequest();
-      request.open('GET', 'http://localhost:3000/people');
-      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-      request.send();
-      request.addEventListener('load', () => {
-         if (request.readyState === 4 && request.status === 200) {
-            let data = JSON.parse(request.response);
-            console.log(data);
+      // const request = new XMLHttpRequest();
+      // request.open('GET', 'http://localhost:3000/people');
+      // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      // request.send();
+      // request.addEventListener('load', () => {
+      //    if (request.readyState === 4 && request.status === 200) {
+      //       let data = JSON.parse(request.response);
+      //       console.log(data);
+      //       createCards(data);
 
-            data.forEach(item => {
-               let card = document.createElement('div');
+      //    } else {
+      //       console.error('Something goes wrong!');
+      //    }
+      // });
 
-               card.classList.add('card');
+      getResource('http://localhost:3000/people')
+         .then(data => createCards(data.data))
+         .catch(err => console.error(err));
 
-               let icon;
-
-               if (item.sex === 'male') {
-                  icon = 'icons/mars.png';
-               } else {
-                  icon = 'icons/female.png';
-               }
-
-               card.innerHTML = `
-                  <img src = ${item.photo} alt = "photo">
-                  <div class="name">${item.name} ${item.surname}</div>
-                  <div class="sex">
-                     <img src=${icon} alt="male">
-                  </div>
-                  <div class="age">${item.age}</div>
-               `;
-
-               document.querySelector('.app').appendChild(card);
-            });
-
-         } else {
-            console.error('Something goes wrong!');
-         }
-      });
       this.remove();
    }
 
@@ -46,4 +27,50 @@ window.addEventListener('DOMContentLoaded', () => {
       'once': true
    });
 
+   // async function getResource(url) {
+   //    const res = await fetch(`${url}`);
+
+   //    if (!res.ok) {
+   //       throw new Error(`Coulnd not fetch ${url}, status: ${res.status}`);
+   //    }
+
+   //    return await res.json();
+   // }
+
+   async function getResource(url) {
+      const res = await axios(`${url}`);
+
+      if (res.status !== 200) {
+         throw new Error(`Coulnd not fetch ${url}, status: ${res.status}`);
+      }
+
+      return res;
+   }
+
+   function createCards(response) {
+      response.forEach(item => {
+         let card = document.createElement('div');
+
+         card.classList.add('card');
+
+         let icon;
+
+         if (item.sex === 'male') {
+            icon = 'icons/mars.png';
+         } else {
+            icon = 'icons/female.png';
+         }
+
+         card.innerHTML = `
+            <img src = ${item.photo} alt = "photo">
+            <div class="name">${item.name} ${item.surname}</div>
+            <div class="sex">
+               <img src=${icon} alt="male">
+            </div>
+            <div class="age">${item.age}</div>
+         `;
+
+         document.querySelector('.app').appendChild(card);
+      });
+   }
 });
