150d79a24b3a0b9b46eb048389f9eef3952275e6
Arlecuno <palchinskij19@gmail.com>
2021-01-04 17:51:07 +0200

    work with Quiz app react

---
+++ js/react/quiz_app/src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,17 @@
+import classes from './AnswersList.module.css';
+import AnswerItem from './AnswerItem/AnswerItem';
+
+export default props => (
+   <ul className={classes.AnswersList}>
+      {props.answers.map((answer, index)=> {
+         return (
+            <AnswerItem 
+               key={index}
+               answer = {answer}
+               onAnswerClick={props.onAnswerClick}
+               state={props.state ? props.state[answer.id] : null}
+            />
+         )
+      })}
+   </ul>
+);
