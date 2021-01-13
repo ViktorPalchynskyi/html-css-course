df6c1ed7c5162f5aa2dce927f0bc5cca98907ccc
Arlecuno <palchinskij19@gmail.com>
2021-01-13 22:04:29 +0200

    study useContext hook

--- js/practice_code/practice_29_react_hook/src/App.js
+++ js/practice_code/practice_29_react_hook/src/App.js
@@ -1,26 +1,18 @@
-import { useState, useCallback} from "react";
-import ItemList from './ItemList';
+import React from "react";
+import Main from './Main';
+import Alert from './alert/Alert';
+import {AlertProvider} from './alert/AlertContext';
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [colored, setColored] = useState(false);
 
-  const styles ={
-    color: colored ? 'darkred' : 'black'
-  };
-
-  const generateItemsFromAPI = useCallback(() => { 
-    return new Array(count).fill('').map((_, i) => `Элемент ${i + 1}`);
-  },[count]);
 
   return(
-    <> 
-     <h1 style={styles}>Количество элементов: {count}</h1>
-      <button className={'btn btn-success'} onClick={()=> setCount(prev => prev + 1)}>Добавить</button>
-      <button className={'btn btn-warning'} onClick={()=> setColored(prev => !prev)}>Изменить</button> 
-
-      <ItemList getItems={generateItemsFromAPI}/>
-    </>
+    <AlertProvider>
+      <div className={"container pt-3"}> 
+        <Alert/>
+        <Main toggle={()=> {}}/>
+      </div>
+    </AlertProvider>
   )
 }
 
---
+++ js/practice_code/practice_29_react_hook/src/alert/Alert.js
@@ -0,0 +1,14 @@
+import {useAlert} from './AlertContext';
+
+export default function Alert() {
+
+   const alert =  useAlert();
+
+   if(!alert.visible) return null;
+
+   return(
+      <div className={"alert alert-danger"} onClick={alert.toggle}>
+         Это очень важное сообщение
+      </div>
+   )
+}
