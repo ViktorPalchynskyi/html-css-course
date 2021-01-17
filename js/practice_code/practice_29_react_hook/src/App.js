a71311bfba713adfb8cc6612e878f7f03f35fe2b
Arlecuno <palchinskij19@gmail.com>
2021-01-17 18:46:58 +0200

    finished react hooks practice project

--- js/practice_code/practice_29_react_hook/src/App.js
+++ js/practice_code/practice_29_react_hook/src/App.js
@@ -1,18 +1,44 @@
-import React from "react";
-import Main from './Main';
-import Alert from './alert/Alert';
-import {AlertProvider} from './alert/AlertContext';
+import React, { useEffect, useState } from "react";
+
+function useLogger(value) {
+  useEffect(()=> { 
+    console.log('Value changed: ', value);
+  }, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = event =>  {
+    setValue(event.target.value);
+  };
+
+  const clear = () => setValue('');
+
+  return{ 
+   bind: { value, onChange},
+   value,
+   clear
+  };
+}
 
 function App() {
 
+  const input = useInput('');
+  const lastName = useInput('');
+
+  useLogger(input.value);
+  useLogger(lastName.value);
 
   return(
-    <AlertProvider>
-      <div className={"container pt-3"}> 
-        <Alert/>
-        <Main toggle={()=> {}}/>
-      </div>
-    </AlertProvider>
+    <div className={"container pt-3"}>
+      <input type="text" {...input.bind}/>
+      <input type="text" {...lastName.bind}/>
+      <hr/>
+      <h1>{input.value} {lastName.value}</h1>
+      <button className="btn btn-warning" onClick={input.clear}>Очистить</button>
+      <button className="btn btn-success" onClick={()=> lastName.clear()}>Очистить2</button>
+    </div>
   )
 }
 
