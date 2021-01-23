153b971b8a9cc8b391859ed2c1c697837bf6d603
Arlecuno <palchinskij19@gmail.com>
2021-01-23 20:48:56 +0200

    study express

--- js/node_js/udemy_course_minin/Module_2/node_express/index.js
+++ js/node_js/udemy_course_minin/Module_2/node_express/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const exphbs= require('express-handlebars');
 const app = express();
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
 
 const hbs = exphbs.create({
    defaultLayout:'main',
@@ -11,28 +14,13 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views','views');
+
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use('/',homeRoutes);
+app.use('/add',addRoutes);
+app.use('/courses',coursesRoutes);
 
-app.get('/', (req,res) => { 
-   res.render('index', {
-      title: 'Главная страница',
-      isHome:true
-   });
-});
-
-app.get('/courses', (req, res) => { 
-   res.render('courses', {
-      title: 'Курсы',
-      isCourses: true
-   });
-});
-
-app.get('/add', (req, res) => { 
-   res.render('add', {
-      title: 'Добавить курсу',
-      isAdd: true
-   });
-});
 
 const PORT = process.env.PORT || 3000;
 
