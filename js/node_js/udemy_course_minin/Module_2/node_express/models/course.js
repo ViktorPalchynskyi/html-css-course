e4537745555d6aa46bc29248d440eec5b3c4ba94
Arlecuno <palchinskij19@gmail.com>
2021-01-25 13:43:59 +0200

    practice with express

--- js/node_js/udemy_course_minin/Module_2/node_express/models/course.js
+++ js/node_js/udemy_course_minin/Module_2/node_express/models/course.js
@@ -1,84 +1,84 @@
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('uuid/v4');
 const fs = require('fs');
 const path = require('path');
 
-class Course{ 
-   constructor(title, price, img) { 
-      this.title = title;
-      this.price = price;
-      this.img = img;
-      this.id = uuidv4();
-   }
+class Course {
+  constructor(title, price, img) {
+    this.title = title;
+    this.price = price;
+    this.img = img;
+    this.id = uuid();
+  }
 
-   toJSON() { 
-      return {
-         title: this.title, 
-         price: this.price,
-         img: this.img,
-         id: this.id
-      };
-   }
+  toJSON() {
+    return {
+      title: this.title,
+      price: this.price,
+      img: this.img,
+      id: this.id
+    }
+  }
 
-   static async update(course) { 
-      const courses = await Course.getAll();
-      
-      const idx = courses.findIndex(c => c.id === course.id);
-      courses[idx] = course;
+  static async update(course) {
+    const courses = await Course.getAll()
 
-      return new Promise((resole, reject) => { 
-         fs.writeFile(
-            path.join(__dirname, '..','data','courses.json'), 
-            JSON.stringify(courses),
-            (err) =>{ 
-               if(err) {
-                  reject(err);
-               } else { 
-                  resole();
-               }
-            }
-         );
-      });
-   }
+    const idx = courses.findIndex(c => c.id === course.id)
+    courses[idx] = course;
 
-   async save(){
-      const courses = await Course.getAll();
-      courses.push(this.toJSON());
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
 
-      return new Promise((resole, reject) => { 
-         fs.writeFile(
-            path.join(__dirname, '..','data','courses.json'), 
-            JSON.stringify(courses),
-            (err) =>{ 
-               if(err) {
-                  reject(err);
-               } else { 
-                  resole();
-               }
-            }
-         );
-      });
-   }
+  async save() {
+    const courses = await Course.getAll();
+    courses.push(this.toJSON());
 
-   static getAll() { 
-      return new Promise((resole, reject) => { 
-         fs.readFile(
-            path.join(__dirname,'..', 'data', 'courses.json'),
-            'utf-8',
-            (err, content) => { 
-               if(err) {
-                  reject(err);
-               } else { 
-                  resole(JSON.parse(content));
-               }   
-            }
-         );
-      });
-   }
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
 
-   static async getById(id) { 
-      const courses = await Course.getAll();
-      return courses.find(c => c.id === id);
-   }
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        'utf-8',
+        (err, content) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(content));
+          }
+        }
+      );
+    });
+  }
+
+  static async getById(id) {
+    const courses = await Course.getAll();
+    return courses.find(c => c.id === id);
+  }
 }
 
 module.exports = Course;
