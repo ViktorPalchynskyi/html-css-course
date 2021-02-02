32a41ded32b887ea913a8fc96af4f0a47ea6665a
Arlecuno <palchinskij19@gmail.com>
2021-02-02 13:32:53 +0200

    created redux store

--- js/node_js/node_practice_project/client/src/App.js
+++ js/node_js/node_practice_project/client/src/App.js
@@ -4,9 +4,13 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () =>
   (
+  <Provider store={store}>
     <Router>
        <>
         <Navbar/>
@@ -19,6 +23,7 @@ const App = () =>
         </section>
       </>
     </Router>
+  </Provider>
   );
 
 export default App;
