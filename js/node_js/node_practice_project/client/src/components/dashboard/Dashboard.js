c541bb8c1836c3688f6de3098fe232537ea93145
Arlecuno <palchinskij19@gmail.com>
2021-02-06 22:20:06 +0200

    add aducation & experience list

--- js/node_js/node_practice_project/client/src/components/dashboard/Dashboard.js
+++ js/node_js/node_practice_project/client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,9 @@ import {connect} from 'react-redux';
 import {getCurrentProfile} from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import {Link} from 'react-router-dom';
-import DashboardActions from '../dashboard/DashboardActions';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
    getCurrentProfile,
@@ -25,6 +27,8 @@ const Dashboard = ({
          {profile !== null ? 
          (<>
             <DashboardActions/>
+            <Experience experience={profile.experience}/>
+            <Education education={profile.education}/> 
          </>) 
             : 
          (<>
