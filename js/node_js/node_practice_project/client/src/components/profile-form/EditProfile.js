a494d4175d209ec9e3d3edfacdbb13ae85be60be
Arlecuno <palchinskij19@gmail.com>
2021-02-07 17:29:35 +0200

    add ProfileTop & ProfileAbout component

--- js/node_js/node_practice_project/client/src/components/profile-form/EditProfile.js
+++ js/node_js/node_practice_project/client/src/components/profile-form/EditProfile.js
@@ -125,6 +125,8 @@ const EditProfile = (
             type="text"
             placeholder="Github Username"
             name="githubusername"
+            value={githubusername} 
+            onChange={e => onChange(e)}
           />
           <small className="form-text"
             >If you want your latest repos and a Github link, include your
@@ -132,7 +134,7 @@ const EditProfile = (
           >
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
+          <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
--- js/node_js/node_practice_project/client/src/components/profile/Profile.js
+++ js/node_js/node_practice_project/client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import {getProfileById} from '../../actions/profile';
 import { Link } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
    match,
@@ -13,7 +15,7 @@ const Profile = ({
 
    useEffect(()=> { 
       getProfileById(match.params.id);
-   }, [getProfileById]);
+   }, [getProfileById, match.params.id]);
 
    return (
       <>
@@ -24,6 +26,10 @@ const Profile = ({
             {auth.isAuthenicated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">
                Edit Profile
             </Link>)}
+            <div className="profile-top bg-primary p-2"> 
+               <ProfileTop profile={profile}/>
+            </div>
+            <ProfileAbout profile={profile}/>
          </>}
       </>
    );
