9663437901d7b2c4ae60f3c609b36f1b9b727fa8
Arlecuno <palchinskij19@gmail.com>
2021-02-07 18:19:00 +0200

    add profile experience & education display

--- js/node_js/node_practice_project/client/src/components/profile/Profile.js
+++ js/node_js/node_practice_project/client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import {getProfileById} from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({
    match,
@@ -26,10 +28,26 @@ const Profile = ({
             {auth.isAuthenicated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">
                Edit Profile
             </Link>)}
-            <div className="profile-top bg-primary p-2"> 
+            <div className="profile-grid my-1"> 
                <ProfileTop profile={profile}/>
+               <ProfileAbout profile={profile}/>
+               <div className="profile-exp bg-white p-2">
+                  <h2 className="text-primary">Experience</h2>
+                  {profile.experience.length > 0 ? (<>
+                     {profile.experience.map(experience => ( 
+                        <ProfileExperience key={experience._id} experience={experience}/>
+                     ))}
+                  </>) : (<h4>No experience credentials</h4>)}
+               </div>
+               <div className="profile-edu bg-white p-2">
+                  <h2 className="text-primary">Education</h2>
+                  {profile.education.length > 0 ? (<>
+                     {profile.education.map(education => ( 
+                        <ProfileEducation key={education._id} education={education}/>
+                     ))}
+                  </>) : (<h4>No education credentials</h4>)}
+               </div>
             </div>
-            <ProfileAbout profile={profile}/>
          </>}
       </>
    );
