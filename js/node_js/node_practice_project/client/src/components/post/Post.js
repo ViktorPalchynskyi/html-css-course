0114c8756698283a0bd9ec8f89a2dfbf1551f65d
Arlecuno <palchinskij19@gmail.com>
2021-02-08 19:31:30 +0200

    add comment display & delete

--- js/node_js/node_practice_project/client/src/components/post/Post.js
+++ js/node_js/node_practice_project/client/src/components/post/Post.js
@@ -5,7 +5,8 @@ import Spinner from '../layout/Spinner';
 import {getPost} from '../../actions/post';
 import PostItem from '../posts/PostItem';
 import { Link } from 'react-router-dom';
-import CommentForm from '../posts/CommentForm';
+import CommentForm from './CommentForm';
+import CommentItem from '../post/CommentItem';
 
 const Post = ({getPost, post: {post, loading}, match}) => {
 
@@ -17,6 +18,11 @@ const Post = ({getPost, post: {post, loading}, match}) => {
       <Link to="/posts" className="btn">Back to posts</Link>
       <PostItem post={post} showActions={false}/>
       <CommentForm postId={post._id}/>
+      <div className="comments">
+         {post.comments.map(comment => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id}/>
+         ))}
+      </div>
    </>
 };
 
