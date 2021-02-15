e946723cf4604dbe814e147fa387ec6fabf38f81
Arlecuno <palchinskij19@gmail.com>
2021-02-15 22:16:17 +0200

    nodejs practice

---
+++ js/practice_code/practice_34/index.js
@@ -0,0 +1,11 @@
+// const func = {
+//     func1: function () {
+//         console.log(this);
+//     },
+//     func2: () => console.log(this)
+// };
+
+// func.func1();
+// func.func2();
+this.table = 'window table';
+console.log(this.table);
