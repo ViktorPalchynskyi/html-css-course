f92af0699054c935ef3f72e59b6d2154e15de5e3
Arlecuno <palchinskij19@gmail.com>
2021-02-21 13:29:10 +0200

    study mongodb methods

--- js/node_js/node_js_practice/practice_11/app.js
+++ js/node_js/node_js_practice/practice_11/app.js
@@ -2,14 +2,26 @@ const MongoClient = require('mongodb').MongoClient;
 
 const mongoClient = new MongoClient('mongodb://localhost:27017/', { useUnifiedTopology: true });
 
-let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}];
+let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}, {name: "Viktor", age: 23},{name: "Tanya", age: 22}]; 
+
 mongoClient.connect((err, client) => {
 
+   if(err) return console.log(err);
+   
    const db = client.db('test');
-   const collection = db.collection('users');
-   collection.findOne({name: 'Alice'},(err, results) => {
+   const col = db.collection('users');
+   col.findOneAndUpdate(
+      {name: 'Bob'},
+      {$set: {name: 'Johnny'}},
+      {returnOriginal: false},
+      (err, result) => { 
 
-      console.log(results);
-      client.close();
-   });
+         if(err) console.log(err);
+         
+
+         console.log(result);
+         client.close();
+      }
+   );
+   
 });
