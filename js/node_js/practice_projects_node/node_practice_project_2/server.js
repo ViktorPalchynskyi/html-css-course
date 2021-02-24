b13fe23ee38036291a19bf94b380c4d9fa4f1a86
Arlecuno <palchinskij19@gmail.com>
2021-02-24 23:32:22 +0200

    practice with http module

--- js/node_js/practice_projects_node/node_practice_project_2/server.js
+++ js/node_js/practice_projects_node/node_practice_project_2/server.js
@@ -1,10 +1,57 @@
 const http = require('http');
 
+const todos = [
+   {id: 1, text: 'Todo One'},
+   {id: 2, text: 'Todo Two'},
+   {id: 3, text: 'Todo Three'},
+];
+
 const server =  http.createServer((req, res) => { 
-  
-   const {headers, url, method} = req;
-   console.log(headers, url, method);
-   res.end('<h1>Hello there</h1>');
+
+   const {method, url} = req;
+
+   let body = [];
+
+   req.on('data', chunk => { 
+      body.push(chunk);
+   }).on('end', () => { 
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+      const response = { 
+         success: false, 
+         data: null, 
+         error: null
+      };
+
+      if(method === 'GET' && url === '/todos') { 
+         status = 200;
+         response.success = true;
+         response.data = todos;
+      } else if(method === 'POST' && url === '/todos') { 
+         const {id, text} = JSON.parse(body);
+
+         if(!id || !text) { 
+            status = 400;
+            response.error = 'please add id and text';
+         } else { 
+            todos.push({id, text});
+            status = 201;
+            response.success = true;
+            response.data = todos;
+         }
+
+      }
+      
+      res.writeHead(status, { 
+         'Content-Type': 'application/json',
+         'X-Powered-By': 'Node.js'
+      });
+
+      res.end(JSON.stringify(response));
+   });   
+
+
 });
 
 server.listen(5000, () => console.log('Server is working...'));
